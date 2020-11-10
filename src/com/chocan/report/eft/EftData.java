e9dfc5aaec7400b7a7784d86c78634c009623d9b
cmfryzel <topherfryzel@gmail.com>
2020-11-10 16:21:19 -0600

    EFTdata changes

--- src/com/chocan/report/eft/EftData.java
+++ src/com/chocan/report/eft/EftData.java
@@ -1,23 +1,23 @@
 package com.chocan.report.eft;
 
-//import com.chocan.report; don't think this is necessary?
 import com.chocan.service.provided.ServiceProvided;
+
+//import com.chocan.report; don't think this is necessary?
 
 //set up file containing provider name, provider number, amount to be transferred
 
 public class EftData {
-	private ServiceProvided[] servicesForWeek; 
+	private ServiceProvided servicesForWeek[]; 
 	private String EFTData;
 	
-	public String produceEFTData(ServiceProvided[] serviceProvidedList) {
+	public String produceEFTData(ServiceProvided servicesForWeek[]) {
 		
-		//what should this class do?
-//		for(int i = 0; i<serviceProvidedList.length; ++i) { //this should find the length of the array?
-//			System.out.print(serviceProvidedList[i]
-//			System.out.print(" ")
-//		}
+		//what should this do?
+		for(int i = 0; i<servicesForWeek.length; ++i) { //this should find the length of the array?
+			EFTData += servicesForWeek[i];
+		}
 		
-		return ;
+		return EFTData;
 	}
 	private void writeProviderName() {
 		//write provider name to the file
