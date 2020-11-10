413b2c461d7ac66dc3bcea4d532d5556bfa0c6fa
bcpeinhardt <benjaminpeinhardt@gmail.com>
2020-11-10 17:40:01 -0600

    more method stubs

--- src/com/chocan/report/member/MemberReportController.java
+++ src/com/chocan/report/member/MemberReportController.java
@@ -1,5 +1,34 @@
 package com.chocan.report.member;
 
+import com.chocan.service.provided.ServiceProvided;
+
 public class MemberReportController {
+
+    private ServiceProvided[] servicesProvided;
+    private String memberReport;
+
+    public String produceMemberReport(ServiceProvided[] serviceProvidedToMemberList) {
+
+    }
+
+    private void writeMemberNameAndNumber() {
+
+    }
+
+    private void writeMemberAddressInfo() {
+
+    }
+
+    private void writeServiceDate() {
+
+    }
+
+    private void writeProviderName() {
+
+    }
+
+    private void writeServiceName() {
+
+    }
 
 }
