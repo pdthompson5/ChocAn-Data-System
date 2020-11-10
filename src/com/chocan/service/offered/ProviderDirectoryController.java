aadc510eb6bd7d2b17d603bcfd42d7f3f86958da
cmfryzel <topherfryzel@gmail.com>
2020-11-10 17:32:47 -0600

    method stubs, provdircont and servprovcont

--- src/com/chocan/service/offered/ProviderDirectoryController.java
+++ src/com/chocan/service/offered/ProviderDirectoryController.java
@@ -3,5 +3,16 @@ package com.chocan.service.offered;
 import com.chocan.service.offered.ServiceOffered;
 
 public class ProviderDirectoryController {
+	private ServiceOffered[] providerDirectory;
+	
+	public ServiceOffered[] getProviderDirectory(){
+		
+	}
+	public ServiceOffered getServiceByName(String serviceName) {
+		
+	}
+	public ServiceOffered getServiceByCode(String serviceCode){
+		
+	}
 
 }
