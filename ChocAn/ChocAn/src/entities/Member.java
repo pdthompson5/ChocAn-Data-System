63d7f2d63e3cd4a1efe34594804b1fea1ac18a35
bcpeinhardt <benjaminpeinhardt@gmail.com>
2020-11-11 10:24:39 -0600

    Member.java implemented

--- ChocAn/ChocAn/src/entities/Member.java
+++ ChocAn/ChocAn/src/entities/Member.java
@@ -1,23 +1,44 @@
 package entities;
 
+/**
+ * Encompasses extra information relevent to Member (memberNumber, memberStatus)
+ * @author Ben Peinhardt
+ */
 public class Member extends SystemUser {
     
+    // Private Attributes
     private String memberNumber;
     private boolean memberStatus;
 
+    /**
+     * Gets member number
+     * @return String memberNumber
+     */
     public String getMemberNumber() {
-
+        return this.memberNumber;
     }
 
+    /**
+     * Gets member status
+     * @return boolean memberStatus
+     */
     public boolean getMemberStatus() {
-
+        return this.memberStatus;
     }
 
+    /**
+     * Sets member number 
+     * @param memberNumber
+     */
     public void setMemberNumber(String memberNumber) {
-
+        this.memberNumber = memberNumber;
     }
 
+    /**
+     * Sets member status
+     * @param memberStatus
+     */
     public void setMemberStatus(boolean memberStatus) {
-        
+        this.memberStatus = memberStatus;
     }
 }
