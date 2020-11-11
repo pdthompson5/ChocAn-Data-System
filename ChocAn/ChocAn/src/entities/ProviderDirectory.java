f511a33bb1153d5d5f36937d3ad8b9fe502fc97b
pdthompson5 <pdthompson5@crimson.ua.edu>
2020-11-11 13:29:25 -0600

    minor added vars

--- ChocAn/ChocAn/src/entities/ProviderDirectory.java
+++ ChocAn/ChocAn/src/entities/ProviderDirectory.java
@@ -7,14 +7,17 @@ public class ProviderDirectory {
     private ArrayList<ServiceOffered> providerDirectory;
 
     public ArrayList<ServiceOffered> getProviderDirectory() {
-
+    	ArrayList<ServiceOffered> list = new ArrayList<ServiceOffered>();
+    	return list;
     }
 
     public ServiceOffered getServiceByName(String serviceName) {
-
+    	ServiceOffered service = new ServiceOffered();
+    	return service;
     }
 
     public ServiceOffered getServiceByCode(String serviceCode) {
-
+    	ServiceOffered service = new ServiceOffered();
+    	return service;
     }
 }
