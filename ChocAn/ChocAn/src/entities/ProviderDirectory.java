7a32536039960be86ba4b49e798288bbe5caa5dc
bcpeinhardt <benjaminpeinhardt@gmail.com>
2020-11-11 15:59:28 -0600

    Implemented Provider Directory

--- ChocAn/ChocAn/src/entities/ProviderDirectory.java
+++ ChocAn/ChocAn/src/entities/ProviderDirectory.java
@@ -2,22 +2,54 @@ package entities;
 
 import java.util.ArrayList;
 
+/**
+ * Encompasses Provider Directory, which is actually a list of Services Offered
+ * @author Ben Peinhardt
+ */
 public class ProviderDirectory {
     
+    // Private attributes
     private ArrayList<ServiceOffered> providerDirectory;
 
+    /**
+     * Gets the entire provider directory
+     * @return ArrayList<ServiceOffered> providerDirectory
+     */
     public ArrayList<ServiceOffered> getProviderDirectory() {
-    	ArrayList<ServiceOffered> list = new ArrayList<ServiceOffered>();
-    	return list;
+    	return this.providerDirectory;
     }
 
+    /**
+     * Returns an entire Service Offered structure based on service name
+     * @param serviceName
+     * @return ServiceOffered service
+     */
     public ServiceOffered getServiceByName(String serviceName) {
-    	ServiceOffered service = new ServiceOffered();
-    	return service;
+    	for (int i = 0; i < this.providerDirectory.size(); i++) {
+            if (this.providerDirectory.get(i).getServiceName() == serviceName) {
+                return this.providerDirectory.get(i);
+            }
+        }
+
+        // Service Not found
+        ServiceOffered temp = new ServiceOffered();
+        return temp;
     }
 
+    /**
+     * Gets service from list by service code
+     * @param serviceCode
+     * @return
+     */
     public ServiceOffered getServiceByCode(String serviceCode) {
-    	ServiceOffered service = new ServiceOffered();
-    	return service;
+    	for (int i = 0; i < this.providerDirectory.size(); i++) {
+            if (this.providerDirectory.get(i).getServiceCode() == serviceCode) {
+                return this.providerDirectory.get(i);
+            }
+        }
+
+        // Service not found
+        ServiceOffered temp = new ServiceOffered();
+        return temp;
     }
 }
