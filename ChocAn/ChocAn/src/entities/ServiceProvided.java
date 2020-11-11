23e007b00c108121975b5de34fde2478007a00cf
bcpeinhardt <benjaminpeinhardt@gmail.com>
2020-11-11 16:34:26 -0600

    Implemented most of ServiceProvided

--- ChocAn/ChocAn/src/entities/ServiceProvided.java
+++ ChocAn/ChocAn/src/entities/ServiceProvided.java
@@ -1,5 +1,10 @@
 package entities;
 
+import java.util.Date;
+
+/**
+ * Encompasses a service provided to a customer 
+ */
 public class ServiceProvided extends ServiceOffered {
 
     // Private attributes
@@ -10,32 +15,69 @@ public class ServiceProvided extends ServiceOffered {
     private Member member;
     private ServiceOffered service;
     private String comments; 
+
+    /**
+     * Constructor: sets attributes of the class
+     * @param service
+     * @param providerNumber
+     * @param memberNumber
+     * @param comments
+     */
+    public void ServiceProvided(ServiceOffered service, Provider provider, Member member, String comments) {
+        this.service = service;
+        this.serviceProvidedID = service.getServiceCode();
+        this.comments = comments;
+        this.member = member;
+        this.provider = provider;
+
+        // TODO: Use imported Date object to set serviceProvidedID and timeProvided
+    }
     
+    /**
+     * Gets the time a service was provided
+     * @return String timeProvided
+     */
     public String getTimeProvided() {
     	return timeProvided;
     } 
 
+    /**
+     * Gets the date a service was provided
+     * @return String date
+     */
     public String getDate() {
     	return date;
     }
 
+    /**
+     * Gets the provider number of the provider who provided the service
+     * @return String providerNumber
+     */
     public String getProviderNumber() {
     	return provider.getProviderNumber();
     }
 
+    /**
+     * Gets the member number of the member who recieved the service
+     * @return String memberNumber
+     */
     public String getMemberNumber() {	
     	return member.getMemberNumber();
     } 
 
+    /**
+     * Gets the entire service object for the type of service provided
+     * @return String service
+     */
     public ServiceOffered getService() {
     	return service;
     }
 
+    /**
+     * Gets the comments from the service provided
+     * @return String comments
+     */
     public String getComments() {
     	return comments;
-    }
-
-    public void ServiceProvided(ServiceOffered service, String providerNumber, String memberNumber, String comments) {
-
     }
 }
