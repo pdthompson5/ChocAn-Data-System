66b56bdcd9df3e7163f9e01e656c900419f39bf7
pdthompson5 <pdthompson5@crimson.ua.edu>
2020-11-13 11:48:41 -0600

    Added some Fuction stubs from Griffin's project

--- ChocAn/ChocAn/src/controllers/EFTDataController.java
+++ ChocAn/ChocAn/src/controllers/EFTDataController.java
@@ -6,4 +6,15 @@ public class EFTDataController {
 	public String produceEFTData() {
 		return "";
 	}
+	
+	private void writeProviderName() {
+		//write provider name to the file
+		
+	}
+	private void writeProviderNumber(){
+		//write provider number to the file
+	}
+	private void writeFeeToBeTransferred() {
+		//write amount to be transferred
+	}
 }
--- ChocAn/ChocAn/src/controllers/ProviderReportController.java
+++ ChocAn/ChocAn/src/controllers/ProviderReportController.java
@@ -11,4 +11,29 @@ public class ProviderReportController {
 	public String produceProviderReport(ArrayList<ServiceProvided> servicesForWeek) {
 		return "";
 	}
+	
+	private void writeMemberNameAndNumber() {
+		
+	}
+	private void writeMemberAddressInfo() {
+		
+	}
+	private void writeServiceDateAndTime() {
+		
+	}
+	private void writeMemberInfo() {
+		
+	}
+	private void writeServiceCode(){
+		
+	}
+	private void writeFeeForService() {
+		
+	}
+	private void writeTotalConsultations() {
+		
+	}
+	private void writeTotalFee() {
+		
+	}
 }
