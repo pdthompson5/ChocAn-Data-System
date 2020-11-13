a5f4e51332818b5ba63940a759aa556b993d75dd
bcpeinhardt <benjaminpeinhardt@gmail.com>
2020-11-12 22:19:13 -0600

    Service Provided List persistance implemented

--- ChocAn/ChocAn/src/entities/ServiceProvidedList.java
+++ ChocAn/ChocAn/src/entities/ServiceProvidedList.java
@@ -2,6 +2,11 @@ package entities;
 
 import java.security.Provider.Service;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 /**
  * Encompasses the operations on the list of Services Provided
@@ -11,6 +16,70 @@ public class ServiceProvidedList {
 
     // Private Attributes
     private ArrayList<ServiceProvided> serviceProvidedList;
+    private String path = "service_provided_list.csv";
+
+    /**
+     * Constructor, builds ServiceProvidedList from csv file
+     */
+    public ServiceProvidedList() {
+        try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String line;
+			while ((line = br.readLine()) != null) {
+			    String[] values = line.split(",");
+                ServiceProvided tempService = buildServiceProvidedFromCSV(values);
+                this.serviceProvidedList.add(tempService);	
+			}
+        br.close();
+
+    	} catch (Exception e) {
+           System.out.println("Unable to read services provided from member file");
+    	}
+    }
+
+    /**
+     * Helper for the Constructor, builds individual ServiceProvided object from array of String values
+     * @param values
+     * @return ServiceProvided myServiceProvided
+     */
+    private ServiceProvided buildServiceProvidedFromCSV(String[] values) {
+        MemberList myMemberList = new MemberList();
+        Member myMember = new Member();
+        ProviderList myProviderList = new ProviderList();
+        Provider myProvider = new Provider();
+        ServiceOffered myServiceOffered = new ServiceOffered();
+        ProviderDirectory myProviderDirectory = new ProviderDirectory();
+
+        // Get the member
+        for (int i = 0; i < myMemberList.getMemberList().size(); i++) {
+            if (myMemberList.getMemberList().get(i).getMemberNumber() == values[4]) {
+                myMember = myMemberList.getMemberList().get(i);
+            }
+        }
+
+        // Get the provider
+        for (int i = 0; i < myProviderList.getProviderList().size(); i++) {
+            if (myProviderList.getProviderList().get(i).getProviderNumber() == values[3]) {
+                myProvider = myProviderList.getProviderList().get(i);
+            }
+        }
+
+        // Get the service
+        for (int i = 0; i < myProviderDirectory.getProviderDirectory().size(); i++) {
+            if (myProviderDirectory.getProviderDirectory().get(i).getServiceCode() == values[5]) {
+                myServiceOffered = myProviderDirectory.getProviderDirectory().get(i);
+            }
+        }
+
+        // Make the service
+        ServiceProvided myServiceProvided = new ServiceProvided(myServiceOffered, myProvider, myMember, values[6]);
+
+        // Reset auto generated Date/Time/ID Values
+        myServiceProvided.setDateTimeID(values[2], values[1], values[0]);
+
+        // Return the serviceProvided
+        return myServiceProvided;
+    }
     
     /**
      * Adds a service provided to the list
@@ -23,6 +92,9 @@ public class ServiceProvidedList {
 
         ServiceProvided newService = new ServiceProvided(service, provider, member, comments);
         this.serviceProvidedList.add(newService);
+
+        // Persist service provided
+        persist();
     }
 
     /**
@@ -110,5 +182,28 @@ public class ServiceProvidedList {
         }
 
         return list;
+    }
+
+    /**
+     * Persists ServiceProvidedList information to a csv file, called whenever list is updated
+     */
+    private void persist() {
+        try {
+            File file = new File(this.path);
+	        FileWriter fw = new FileWriter(file);
+	        PrintWriter pw = new PrintWriter(fw);
+
+	        for (int i = 0; i < this.serviceProvidedList.size(); i++) {
+	            pw.print(this.serviceProvidedList.get(i).writeServiceProvidedToCSV());
+	            if (i != this.serviceProvidedList.size() - 1) {
+	                pw.println(" ");
+	            }
+	        }
+	        pw.println(" ");
+	
+	        pw.close();
+        } catch(Exception e) {
+            System.out.println("Unable to persist service provided information");
+        }
     }
 }
